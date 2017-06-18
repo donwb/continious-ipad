a63a8850a553659fe5de111ece7fd9cf2eaa93b5
Don Browning <osdev@me.com>
2017-06-18 00:46:32 -0400

    test

---
+++ App.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace test
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            
+        }
+    }
+}
